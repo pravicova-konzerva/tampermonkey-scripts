6ae75446ed259e2a05f4a8f3735fdda54c7238a5
ministerstvopravdy <ministerstvopravdycz@gmail.com>
2024-09-09 12:43:58 +0200

    Create Idnes-clipboard-cleanse.js

---
+++ Idnes-clipboard-cleanse.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Idnes clipboard cleanse
+// @version      2024-09-06
+// @description  Cleans clipboard when copying text on idnes.cz from their garbage
+// @author       Ministerstvopravdy
+// @match        https://www.idnes.cz/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+setTimeout(() => {
+  window.Copylink={}
+}, 1000);
+
+})();
